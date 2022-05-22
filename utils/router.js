ac79a4e73424a46d4e836231612047aeb5971537
Kam <kamaran.layne@gmail.com>
2022-05-22 16:30:16 -0400

    lint router

--- utils/router.js
+++ utils/router.js
@@ -1,6 +1,8 @@
-import UserRouter from '../routes/userRouter.js'
-import GroupRouter from '../routes/groupRouter.js'
-import express from 'express'
+"use-strict";
+
+import UserRouter from "../routes/userRouter.js"
+import GroupRouter from "../routes/groupRouter.js"
+import express from "express"
 
 let appRouter = express.Router({ mergeParams: true });
 
@@ -8,11 +10,7 @@ const userRouter = new UserRouter(),
   groupRouter = new GroupRouter();
 
 appRouter
-  .use((req, res, next) => {
-    //
-    next()
-  })
-  .use('/user', userRouter.routes)
-  .use('/group', groupRouter.routes)
+  .use("/user", userRouter.routes)
+  .use("/group", groupRouter.routes);
 
-export default appRouter
+export default appRouter;
