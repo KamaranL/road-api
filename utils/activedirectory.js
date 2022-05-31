2fd7c3c19c2b3a93be5c10c6f04587d3bb9e0b2d
Kam <kamaran.layne@gmail.com>
2022-05-31 07:59:18 -0400

    implemented TLS binding for ldaps

--- utils/activedirectory.js
+++ utils/activedirectory.js
@@ -5,18 +5,35 @@ import { env } from "process";
 import fs from "node:fs";
 import ActiveDirectory from "activedirectory2";
 
+const tls = {
+
+  enforce: env.AD2_TLS_ENFORCE !== "true" ? false : true,
+
+  options: () => {
+    if (env.AD2_TLS !== "true") return null;
+    else {
+      return {
+        key: fs.readFileSync(env.AD2_TLS_KEY),
+        cert: fs.readFileSync(env.AD2_TLS_CERT),
+        ca: [fs.readFileSync(env.AD2_TLS_CA)],
+        rejectUnauthorized: tls.enforce,
+      };
+    }
+  },
+
+  port: env.AD2_TLS !== "true" ? 389 : 636,
+
+  protocol: env.AD2_TLS !== "true" ? "ldap" : "ldaps",
+
+};
+
 const activeDirectory = new ActiveDirectory({
 
-  url: `${env.AD2_PROTOCOL}${env.AD2_HOST}:${env.AD2_PORT}`,
+  url: `${tls.protocol}://${env.AD2_HOST}:${tls.port}`,
   baseDN: env.AD2_BASE,
   username: env.AD2_USER,
   password: env.AD2_PASS,
-  tlsOptions: {
-    key: fs.readFileSync(env.AD2_TLS_KEY),
-    cert: fs.readFileSync(env.AD2_TLS_CERT),
-    ca: [fs.readFileSync(env.AD2_TLS_CA)],
-    rejectUnauthorized: true,
-  },
+  tlsOptions: tls.options(),
   attributes: {
     user: [],
     group: ["dn", "cn", "description", "member", "distinguishedName", "objectCategory"],
